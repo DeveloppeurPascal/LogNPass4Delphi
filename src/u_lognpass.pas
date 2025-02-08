441a140f52b8ed7f4f56611dea0b68cddced9a51
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-08 17:47:21 +0100

    added a timestamp and signature comment

--- src/u_lognpass.pas
+++ src/u_lognpass.pas
@@ -1,3 +1,39 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Log'n Pass for Delphi
+///
+/// Copyright 2016-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Delphi client library for the Log'n Pass OTP service.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://lognpass4delphi.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/LogNPass4Delphi
+///
+/// ***************************************************************************
+/// File last update : 2025-02-05T20:59:31.469+01:00
+/// Signature : d41f96b699b120e9d870c03c36784113204b2246
+/// ***************************************************************************
+/// </summary>
+
 unit u_lognpass;
 
 interface
