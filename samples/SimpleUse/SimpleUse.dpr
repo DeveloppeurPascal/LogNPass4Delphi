91294fbdb260234c8ff68776ab8ccfa47bc2301a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-26 15:57:17 +0200

    updated C2PP comments

--- samples/SimpleUse/SimpleUse.dpr
+++ samples/SimpleUse/SimpleUse.dpr
@@ -1,38 +1,38 @@
-﻿/// <summary>
-/// ***************************************************************************
-///
-/// Log'n Pass for Delphi
-///
-/// Copyright 2016-2025 Patrick PREMARTIN under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Delphi client library for the Log'n Pass OTP service.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://lognpass4delphi.developpeur-pascal.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/LogNPass4Delphi
-///
-/// ***************************************************************************
-/// File last update : 2025-02-08T17:35:52.000+01:00
-/// Signature : 5506fbb22f41e01b5160b55a678c02b2d793d90f
-/// ***************************************************************************
-/// </summary>
+﻿(* C2PP
+  ***************************************************************************
+
+  Log'n Pass for Delphi
+
+  Copyright 2016-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Delphi client library for the Log'n Pass OTP service.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://lognpass4delphi.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/LogNPass4Delphi
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:04:01.707+01:00
+  Signature : 3cd291e399b3eefda990257e3097f5e54bde7892
+  ***************************************************************************
+*)
 
 program SimpleUse;
 
--- samples/SimpleUse/fMain.pas
+++ samples/SimpleUse/fMain.pas
@@ -1,38 +1,38 @@
-/// <summary>
-/// ***************************************************************************
-///
-/// Log'n Pass for Delphi
-///
-/// Copyright 2016-2025 Patrick PREMARTIN under AGPL 3.0 license.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-/// DEALINGS IN THE SOFTWARE.
-///
-/// ***************************************************************************
-///
-/// Delphi client library for the Log'n Pass OTP service.
-///
-/// ***************************************************************************
-///
-/// Author(s) :
-/// Patrick PREMARTIN
-///
-/// Site :
-/// https://lognpass4delphi.developpeur-pascal.fr/
-///
-/// Project site :
-/// https://github.com/DeveloppeurPascal/LogNPass4Delphi
-///
-/// ***************************************************************************
-/// File last update : 2025-02-08T17:34:46.000+01:00
-/// Signature : 8e82755700a22dcdbf46f7425933fd6ff11aa0e1
-/// ***************************************************************************
-/// </summary>
+(* C2PP
+  ***************************************************************************
+
+  Log'n Pass for Delphi
+
+  Copyright 2016-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Delphi client library for the Log'n Pass OTP service.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://lognpass4delphi.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/LogNPass4Delphi
+
+  ***************************************************************************
+  File last update : 2025-02-09T11:04:01.707+01:00
+  Signature : eca7dac447432eef5e8308952de63f46a71d78bb
+  ***************************************************************************
+*)
 
 unit fMain;
 
